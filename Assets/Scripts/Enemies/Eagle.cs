155f60702c0f19a4d91c7e8771907b222b1128cf
eduhlira4 <eduardu.silva@hotmail.com>
2022-07-15 11:28:08 -0400

    New Enemy
    - Added the enemy: Eagle
    - Added the eagle Movement and Colision.

--- Assets/Scripts/Enemies/Eagle.cs
+++ Assets/Scripts/Enemies/Eagle.cs
@@ -1,39 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Eagle : MonoBehaviour
 {
     private float dirX;
     private float moveSpeed;
     private Rigidbody2D rb;
+    private bool facingRight = false;
     private Vector3 localScale;
 
-    private IEnumerator coroutine;
+    public string VictoryScene;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        localScale = transform.localScale;
         rb = GetComponent<Rigidbody2D>();
         dirX = -1f;
         moveSpeed = 3f;
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Wall>())
+        {
+            dirX *= -1f;
+        }
+        if (collision.CompareTag("Player"))
+        {
+            //gameController.ActivateVictoryPanel();
+            SceneManager.LoadScene(VictoryScene);
+
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        coroutine = WaitAndPrint(5.0f);
-        StartCoroutine(coroutine);
-        
+
     }
     void FixedUpdate()
     {
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
     }
 
-    private IEnumerator WaitAndPrint(float waitTime)
+    void LateUpdate()
     {
-        yield return new WaitForSeconds(waitTime);
-        dirX = dirX * -1f;
+        CheckWhereToFace();
     }
+
+    void CheckWhereToFace()
+    {
+        if (dirX > 0)
+            facingRight = true;
+        else if (dirX < 0)
+            facingRight = false;
+
+        if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+            localScale.x *= -1;
+
+        transform.localScale = localScale;
+    }
+
+    
 }
